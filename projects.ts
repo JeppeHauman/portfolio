b4599412708cd864e88f5952a11a00fcedbb5b99
Jeppe Hauman <jeppe_hauman@hotmail.com>
2023-05-25 15:35:06 +0200

    init

---
+++ projects.ts
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    title: "Newsmap",
+    description:
+      "Frontend containing an interactive worldmap giving you the latest news from the country you choose.",
+    img: "/News-Map.png",
+    tech: ["Vite", "React", "Newscatcher"],
+  },
+  {
+    title: "Reis Celik",
+    description:
+      "A website made for the turkish film instructor Reis Celik, showing his previous work and awards",
+    img: "/reis-celik.png",
+    tech: ["Nextjs", "Tailwindcss", "Typescript"],
+  },
+  {
+    title: "Leashes",
+    description: "Fullstack application for an ecommerce selling dog leashes",
+    img: "/Leashes.png",
+    tech: ["Nextjs", "Tailwindcss", "Jotai", "Typescript"],
+  },
+  {
+    title: "Change Ahead",
+    description:
+      "A website made for the London-based NGO Change Ahead. An organisation dedicated to preventing homelessness and supporting those in need.",
+    img: "/changeahead.png",
+    tech: ["Wordpress.................... 0.o"],
+  },
+];
